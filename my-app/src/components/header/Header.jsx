c48deb889d2be4a142fcd8dfa8f39a5d83fa21c6
aaribyanareg <gao.22@uni-dubna.ru>
2024-04-05 15:34:24 +0300

    create app

---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return(
+        <div>
+            <h1>
+                Create  App
+            </h1>
+            <p>description</p>
+            <nav>
+                <a href="#">One</a>
+                <a href="#">Two</a>
+                <a href="#">Three</a>
+            </nav>
+            </div>
+    );
+}
+
+export default Header
